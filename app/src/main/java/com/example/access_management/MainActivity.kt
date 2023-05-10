39e6c54269487dec76bf7eca039ebb3836d7f26b
Danny de Jonge <dannydejonge1796@gmail.com>
2023-05-10 13:29:26 +0200

    changed primary color

--- app/src/main/java/com/example/access_management/MainActivity.kt
+++ app/src/main/java/com/example/access_management/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.access_management
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.access_management.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -29,12 +28,6 @@ class MainActivity : AppCompatActivity() {
     val navController = findNavController(R.id.nav_host_fragment_content_main)
     appBarConfiguration = AppBarConfiguration(navController.graph)
     setupActionBarWithNavController(navController, appBarConfiguration)
-
-    binding.fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        .setAnchorView(R.id.fab)
-        .setAction("Action", null).show()
-    }
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
