95107093aa8121ad93155810e6734bf981f4637b
Danny de Jonge <dannydejonge1796@gmail.com>
2023-05-10 14:08:05 +0200

    login fragment form aangemaakt

--- app/src/main/java/com/example/access_management/LoginFragment.kt
+++ app/src/main/java/com/example/access_management/LoginFragment.kt
@@ -1,19 +1,18 @@
 package com.example.access_management
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.access_management.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import com.example.access_management.databinding.FragmentLoginBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class LoginFragment : Fragment() {
 
-  private var _binding: FragmentFirstBinding? = null
+  private var _binding: FragmentLoginBinding? = null
 
   // This property is only valid between onCreateView and
   // onDestroyView.
@@ -24,7 +23,7 @@ class FirstFragment : Fragment() {
     savedInstanceState: Bundle?
   ): View {
 
-    _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    _binding = FragmentLoginBinding.inflate(inflater, container, false)
     return binding.root
 
   }
@@ -32,9 +31,9 @@ class FirstFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
-    binding.buttonFirst.setOnClickListener {
-      findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-    }
+//    binding.buttonFirst.setOnClickListener {
+//      findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//    }
   }
 
   override fun onDestroyView() {
