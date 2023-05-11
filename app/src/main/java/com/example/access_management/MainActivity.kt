f2197669027b4f78deb046e462b3ee9ba95fc97e
Danny de Jonge <dannydejonge1796@gmail.com>
2023-05-11 14:46:56 +0200

    user can now logout

--- app/src/main/java/com/example/access_management/MainActivity.kt
+++ app/src/main/java/com/example/access_management/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.access_management
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,5 +16,12 @@ class MainActivity : AppCompatActivity() {
     val lastName = intent.getStringExtra("lastname")
     val welcomeMessage = getString(R.string.welcome_message, firstName, lastName)
     txtWelcome.text = welcomeMessage
+
+    val btnLogout: Button = findViewById(R.id.btnLogout)
+    btnLogout.setOnClickListener {
+      val intent = Intent(this, LoginActivity::class.java)
+      startActivity(intent)
+      finish()
+    }
   }
 }
