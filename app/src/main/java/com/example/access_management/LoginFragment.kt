320266b0e146c2b5d06c2db080319c95ea429e4f
Danny de Jonge <dannydejonge1796@gmail.com>
2023-05-11 13:57:33 +0200

    blocking account after 3 failed logins

--- app/src/main/java/com/example/access_management/LoginFragment.kt
+++ app/src/main/java/com/example/access_management/LoginFragment.kt
@@ -1,12 +1,18 @@
 package com.example.access_management
 
+import android.content.ContentValues.TAG
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.access_management.databinding.FragmentLoginBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
@@ -29,6 +35,115 @@ class LoginFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
+    binding.btnLogin.setOnClickListener {
+      //Validation status
+      var isFormValid = true
+
+      //Get values from text fields
+      val username = binding.tfUser.text.toString()
+      val password = binding.tfPass.text.toString()
+
+      //If username field is empty
+      if (username.isEmpty()) {
+        //Set field error
+        binding.tfUser.error = "Please enter your username"
+        //Set validation status
+        isFormValid = false
+      }
+
+      //If password field is empty
+      if (password.isEmpty()) {
+        //Set field error
+        binding.tfPass.error = "Please enter your password"
+        //Set validation status
+        isFormValid = false
+      }
+
+      //Check if form is successfully validated
+      if (!isFormValid) {
+        //Display error
+        Toast.makeText(this.requireContext(), "Please correct the errors above", Toast.LENGTH_SHORT).show()
+      } else {
+        //Init firestore database
+        val db = Firebase.firestore
+        //Call db collection
+        val collectionRef = db.collection("users")
+        //Get the document which matches the filled in username
+        collectionRef.whereEqualTo("username", username)
+          .get()
+          .addOnSuccessListener { documents ->
+            if (documents.size() > 0) {
+              val document = documents.first()
+
+              if (document.getLong("login_failures")?.equals(3L) == true) {
+                Toast.makeText(this.requireContext(), "Your account has been blocked", Toast.LENGTH_SHORT).show()
+              } else {
+                //Check if the filled in password matches the password in the database
+                if (password == document.get("password")) {
+                  Toast.makeText(this.requireContext(), "Login", Toast.LENGTH_SHORT).show()
+                } else {
+                  val docRef = collectionRef.document(document.id)
+
+                  //Add 1 to the existing login failures
+                  val loginFailures = (document.getLong("login_failures")?.toInt() ?: 0) + 1
+
+                  //Update the value in the database
+                  docRef.update("login_failures", loginFailures)
+                    .addOnSuccessListener {
+                      Log.d(TAG, "Document updated successfully.")
+                    }
+                    .addOnFailureListener { e ->
+                      Log.w(TAG, "Error updating document.", e)
+                    }
+
+                  //Display failures left message
+                  when (3 - loginFailures) {
+                    2 -> {
+                      Toast.makeText(
+                        this.requireContext(),
+                        "Login failed, you have 2 failures left", Toast.LENGTH_SHORT
+                      ).show()
+                    }
+
+                    1 -> {
+                      Toast.makeText(
+                        this.requireContext(),
+                        "Login failed, you have 1 failure left", Toast.LENGTH_SHORT
+                      ).show()
+                    }
+
+                    0 -> {
+                      //Get the current timestamp
+                      val calendar = Calendar.getInstance()
+                      val timestamp = calendar.time
+
+                      //Set the timestamp for blocked_at in the database
+                      docRef.update("blocked_at", timestamp)
+                        .addOnSuccessListener {
+                          Log.d(TAG, "Document updated successfully.")
+                        }
+                        .addOnFailureListener { e ->
+                          Log.w(TAG, "Error updating document.", e)
+                        }
+
+                      Toast.makeText(
+                        this.requireContext(),
+                        "Login failed, your account has been blocked", Toast.LENGTH_SHORT
+                      ).show()
+                    }
+                  }
+                }
+              }
+            } else {
+              Toast.makeText(this.requireContext(), "Unknown username", Toast.LENGTH_SHORT).show()
+            }
+          }
+          .addOnFailureListener { exception ->
+            Log.d(TAG, "Error getting documents: ", exception)
+          }
+      }
+    }
+
     binding.btnRegister.setOnClickListener {
       findNavController().navigate(R.id.action_LoginFragment_to_RegisterFragment)
     }
@@ -39,3 +154,4 @@ class LoginFragment : Fragment() {
     _binding = null
   }
 }
+
